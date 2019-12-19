9834e4f58ad4f090bce6943f88f775b38f994e69
Your Namect git config --global user.email <roma.chikhladze777@gmail.com>
2019-12-20 00:57:06 +0400

    a

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 const imagePreview = document.getElementById('imagePreview');
+const image = document.querySelector('.image');
 
 const loadFile = function(fileInput) {
     const arr = ['image/png', 'image/jpg', 'image/jpeg'];
     const files = fileInput.files;
     for(file of files) { 
         if(arr.includes(file.type)) {
-           const imageUrl =  window.URL.createObjectURL(file)
-                const image = document.createElement('img');
-                image.setAttribute('class', 'img');
-                image.setAttribute('src', imageUrl);
-                imagePreview.appendChild(image);
+           const imageUrl =  window.URL.createObjectURL(file);
+
+           image.style.backgroundImage = "url("+imageUrl+")";
+               
         }
     }
 };
+
+const real = document.querySelector('real');
+const custom = document.querySelector('custom');
+
+console.log(real);
+
+
+custom.addEventListener('click', function() {
+    // real.click();
+    console.log('object');
+});
 
